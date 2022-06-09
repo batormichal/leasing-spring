1bae0e12a52a5101dc263c0db2bc2c7b5ee45208
michal_bator <michalbator1996@gmail.com>
2022-06-09 08:26:45 +0200

    cors, unused dependencies

---
+++ src/main/java/com/app/leasing/Security.java
@@ -0,0 +1,14 @@
+package com.app.leasing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Security implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
