0a448d8de5cf5e9c323b4981094abfe724e2522e
michal_bator <michalbator1996@gmail.com>
2022-06-09 08:21:42 +0200

    Initial commit

---
+++ src/test/java/com/app/leasing/LeasingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.leasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
